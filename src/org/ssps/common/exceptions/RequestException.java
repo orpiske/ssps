70e24e138ba20088bc928b93bf6745dcec60a835
Otavio Piske <angusyoung@gmail.com>
2012-05-21 23:40:18 -0300

    Initial import

---
+++ src/org/ssps/common/exceptions/RequestException.java
@@ -0,0 +1,19 @@
+package org.ssps.common.exceptions;
+
+/** 
+ * Request related exception
+ * @author Otavio R. Piske <angusyoung@gmail.com>
+ *
+ */
+public class RequestException extends SspsException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RequestException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/org/ssps/common/exceptions/SspsException.java
@@ -0,0 +1,13 @@
+package org.ssps.common.exceptions;
+
+
+public class SspsException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SspsException(final String message) {
+		super(message);
+	}
+}
