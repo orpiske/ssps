8dfac6505e89791f39b93509a33dd72568c1b38b
Otavio Piske <angusyoung@gmail.com>
2012-06-11 11:59:45 -0300

    Renamed the project to spm so that it is simpler and reflect the new  design change

---
+++ src/org/ssps/spm/main/Main.java
@@ -0,0 +1,152 @@
+/**
+   Copyright 2012 Otavio Rodolfo Piske
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.ssps.spm.main;
+
+import java.io.FileNotFoundException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.configuration.ConfigurationException;
+import org.ssps.common.configuration.ConfigurationWrapper;
+import org.ssps.common.logger.LoggerUtils;
+import org.ssps.spm.archive.Archiver;
+import org.ssps.spm.utils.Constants;
+
+public class Main {
+    private static Options options;
+
+    public static void initLogger() throws FileNotFoundException {
+	LoggerUtils.initLogger(Constants.SPM_CONFIG_DIR);
+    }
+
+    /**
+     * Initializes the configuration object
+     */
+    private static void initConfig() {
+	try {
+	    ConfigurationWrapper.initConfiguration(
+		    Constants.SPM_CONFIG_DIR, Constants.CONFIG_FILE_NAME);
+	} catch (FileNotFoundException e) {
+	    System.err.println(e.getMessage());
+	    System.exit(-3);
+	} catch (ConfigurationException e) {
+	    System.err.println(e.getMessage());
+	    System.exit(-3);
+	}
+    }
+
+    public static void help(int code) {
+	HelpFormatter formatter = new HelpFormatter();
+
+	formatter.printHelp("spm", options);
+	System.exit(code);
+    }
+
+    public static CommandLine processCommand(String[] args)
+	    throws ParseException {
+	// create the command line parser
+	CommandLineParser parser = new PosixParser();
+
+	// create the Options
+	options = new Options();
+
+	options.addOption("h", "help", false, "prints the help");
+	options.addOption("c", "create", false, "create the deliverable file");
+	options.addOption("f", "file", true, "path to the DBM file");
+	options.addOption("p", "publish", false,
+		"send the file to the publication server");
+	options.addOption("d", "deliverable", true,
+		"path to the deliverable file");
+	options.addOption("g", "get", false,
+		"gets a deliverable");
+	options.addOption("D", "destination", true,
+		"destination folder for the deliverable");
+	
+	options.addOption("G", "group", true, "group");
+	options.addOption("N", "name", true, "name");
+	options.addOption("V", "version", true, "version");
+
+	return parser.parse(options, args);
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	try {
+	    CommandLine cmdLine = processCommand(args);
+
+	    initLogger();
+	    initConfig();
+
+	    if (cmdLine.hasOption('h')) {
+		help(1);
+	    }
+
+	    if (cmdLine.hasOption('c')) {
+		if (!cmdLine.hasOption('f')) {
+		    help(-1);
+		}
+
+		String dbmFile = cmdLine.getOptionValue('f');
+
+		Archiver archiver = new Archiver(dbmFile);
+
+		archiver.createArchive();
+	    } else {
+		if (cmdLine.hasOption('p')) {
+		    String deliverable = cmdLine.getOptionValue('d');
+		    String dbmFile = cmdLine.getOptionValue('f');
+		
+		    PublicationManager manager = new PublicationManager(dbmFile);
+		    
+		    manager.upload(deliverable);
+		}
+		else {
+		    if (cmdLine.hasOption('g')) {
+			PublicationManager manager = new PublicationManager();
+			
+			String group = cmdLine.getOptionValue('G');
+			String name = cmdLine.getOptionValue('N');
+			String version = cmdLine.getOptionValue('V');
+			String destination = cmdLine.getOptionValue('D');
+			
+			manager.download(group, name, version, destination);
+		    }
+		    else {
+			help(1);
+		    }
+		}
+	    }
+
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+
+	    System.exit(-1);
+	} catch (Exception e) {
+	    System.err
+		    .println("Unable to execute operation: " + e.getMessage());
+
+	    e.printStackTrace();
+	}
+
+    }
+
+}
---
+++ src/org/ssps/spm/main/PublicationManager.java
@@ -0,0 +1,186 @@
+/**
+   Copyright 2012 Otavio Rodolfo Piske
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.ssps.spm.main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
+import org.ssps.common.configuration.ConfigurationWrapper;
+import org.ssps.common.xml.exceptions.XmlDocumentException;
+import org.ssps.spm.archive.dbm.DbmDocument;
+
+import com.googlecode.sardine.DavResource;
+import com.googlecode.sardine.Sardine;
+import com.googlecode.sardine.SardineFactory;
+
+/**
+ * @author Otavio R. Piske <angusyoung@gmail.com>
+ * 
+ */
+public class PublicationManager {
+
+    private static final Logger logger = Logger
+	    .getLogger(PublicationManager.class);
+    private static final PropertiesConfiguration config = ConfigurationWrapper
+	    .getConfig();
+
+    private DbmDocument dbmDocument;
+    private Sardine sardine;
+
+    private String url;
+
+    public PublicationManager() {
+	String repositoryUser = config.getString("default.repository.username");
+	String repositoryPassword = config
+		.getString("default.repository.password");
+
+	url = config.getString("default.repository.url");
+	sardine = SardineFactory.begin(repositoryUser, repositoryPassword);
+    }
+
+    public PublicationManager(final String dbmFile) throws XmlDocumentException {
+	dbmDocument = new DbmDocument(dbmFile);
+
+	String repositoryUser = dbmDocument.getRepositoryUser();
+	String repositoryPassword = dbmDocument.getRepositoryPassword();
+
+	if (repositoryUser == null) {
+	    repositoryUser = config.getString("default.repository.username");
+	    repositoryPassword = config
+		    .getString("default.repository.password");
+	}
+
+	if (repositoryPassword == null) {
+	    repositoryPassword = "";
+	}
+
+	url = dbmDocument.getRepositoryUrl();
+
+	if (url == null) {
+	    url = config.getString("default.repository.url");
+	}
+	sardine = SardineFactory.begin(repositoryUser, repositoryPassword);
+    }
+
+    private String getGroupRoot(final String group) {
+	StringBuffer buffer = new StringBuffer(url);
+
+	buffer.append("/");
+	buffer.append(group);
+
+	return buffer.toString();
+    }
+
+    private String getGroupRoot() {
+	return getGroupRoot(dbmDocument.getProjectGroup());
+    }
+
+    private String getNameRoot(final String group, final String name) {
+	StringBuffer buffer = new StringBuffer(url);
+
+	buffer.append("/");
+	buffer.append(group);
+	buffer.append("/");
+	buffer.append(name);
+
+	return buffer.toString();
+    }
+
+    private String getNameRoot() {
+	return getNameRoot(dbmDocument.getProjectGroup(),
+		dbmDocument.getProjectName());
+    }
+
+    private String getPath(final String group, final String name,
+	    final String version) {
+	StringBuffer buffer = new StringBuffer(url);
+
+	buffer.append("/");
+	buffer.append(group);
+	buffer.append("/");
+	buffer.append(name);
+	buffer.append("/");
+	buffer.append(version);
+	buffer.append("/");
+
+	return buffer.toString();
+    }
+
+    private String getPath() {
+	return getPath(dbmDocument.getProjectGroup(),
+		dbmDocument.getProjectName(), dbmDocument.getProjectVersion());
+    }
+
+    public void upload(final String filename) throws IOException {
+	InputStream stream = new FileInputStream(filename);
+
+	// byte[] data = FileUtils.readFileToByteArray(new File(filename));
+
+	logger.info("Checking if project group directory exists");
+	if (!sardine.exists(getGroupRoot())) {
+	    logger.info("Project group directory does not exists. Creating it ...");
+	    sardine.createDirectory(getGroupRoot());
+	}
+
+	logger.info("Checking if project directory exists");
+	if (!sardine.exists(getNameRoot())) {
+	    logger.info("Project directory does not exists. Creating it ...");
+	    sardine.createDirectory(getNameRoot());
+	}
+
+	logger.info("Checking if deliverable directory exists");
+	if (!sardine.exists(getPath())) {
+	    logger.info("Deliverable directory does not exists. Creating it ...");
+	    sardine.createDirectory(getPath());
+	}
+
+	sardine.put(getPath() + FilenameUtils.getName(filename), stream);
+    }
+
+    public void download(final String group, final String name,
+	    final String version, final String destination) throws IOException {
+	String path = getPath(group, name, version);
+
+	List<DavResource> resources = sardine.list(path);
+
+	for (DavResource resource : resources) {
+
+	    if (!resource.isDirectory()) {
+		File newFile = new File(destination
+			+ File.separator + resource.getName());
+		
+		if (newFile.exists()) {
+		    continue;
+		}
+		else {
+		    newFile.createNewFile();
+		}
+		
+		InputStream stream = new FileInputStream(newFile);
+		stream = sardine.get(resource.getHref().toString());
+
+		stream.close();
+	    }
+	}
+
+    }
+}
