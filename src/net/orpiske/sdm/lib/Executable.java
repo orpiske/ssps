a23a08423b245a9874c5f72b4fc9030bb3c164e2
Otavio R. Piske <angusyoung@gmail.com>
2013-02-25 23:33:39 -0300

    Cleaned up the code by removing legacy XML-based rules and replacing with Groovy-based/support code

---
+++ src/net/orpiske/sdm/lib/Executable.java
@@ -0,0 +1,53 @@
+/**
+   Copyright 2012 Otavio Rodolfo Piske
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package net.orpiske.sdm.lib;
+
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+
+/**
+ * @author Otavio R. Piske <angusyoung@gmail.com>
+ *
+ */
+public class Executable {
+	
+
+	/**
+	 * Executes a command
+	 * @param command the command to execute
+	 * @param arguments the arguments to pass to the command
+	 * @return the return code from the command
+	 * @throws ExecuteException
+	 * @throws IOException
+	 */
+	public static int exec(final String command, final String arguments) throws ExecuteException, IOException {
+		CommandLine cmd = new CommandLine(command);
+		
+		cmd.addArguments(arguments);
+		
+		DefaultExecutor executor = new DefaultExecutor();
+		
+		int exitValue = executor.execute(cmd);
+		return exitValue;
+		
+	}
+
+
+
+}
---
+++ src/net/orpiske/sdm/lib/io/IOUtil.java
@@ -0,0 +1,144 @@
+/**
+   Copyright 2012 Otavio Rodolfo Piske
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package net.orpiske.sdm.lib.io;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.orpiske.sdm.lib.PrintUtils;
+import net.orpiske.sdm.lib.io.support.ShieldAwareCopier;
+import net.orpiske.sdm.lib.io.support.ShieldUtils;
+import net.orpiske.ssps.common.variables.VariablesParser;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Implements the copy rule.
+ * 
+ * @author Otavio R. Piske <angusyoung@gmail.com>
+ *
+ */
+public class IOUtil {
+	
+	private VariablesParser admVariables = VariablesParser.getInstance();
+
+
+	
+	private static void createParentDirectories(File dir) {
+		if (!dir.exists()) {
+			dir.getParentFile().mkdirs();
+		}
+	}
+	
+	
+	/**
+	 * Protects a file or resource from being written
+	 * @param resource the resource to protect
+	 * @throws IOException
+	 */
+	public static void shield(final String resource) throws IOException {
+		File shieldedFile = new File(resource);
+		
+		if (!shieldedFile.exists()) {
+			PrintUtils.printInfo("Resource " + resource + " does not exist");
+			
+			return;
+		}
+		
+		File shielded = new File(resource + ShieldUtils.SHIELD_EXT);
+	
+		if (!shielded.exists()) {
+			
+			shielded.createNewFile();
+				
+			PrintUtils.printInfo("Resource " + resource + " was shielded");
+		}
+		else {
+			PrintUtils.printInfo("Resource " + resource + " already shielded");
+		}
+		
+		shielded.deleteOnExit();
+	}
+	
+	
+	/**
+	 * Copies a file or directory
+	 * @param from the source file or directory
+	 * @param to the destination file or directory
+	 * @throws IOException if the source is not found or there are access restrictions 
+	 */
+	public static void copy(final String from, final String to) throws IOException {
+		copy(from, to, false);
+	}
+	
+	
+	/**
+	 * Copies a file or directory
+	 * @param from the source file or directory
+	 * @param to the destination file or directory
+	 * @param overwrite whether or not to overwrite the destination file/directory
+	 * @throws IOException if the source is not found or there are access restrictions 
+	 */
+	public static void copy(final String from, final String to, boolean overwrite) throws IOException {
+		File fromFile = new File(from);
+		File toFile = new File(to);
+		
+		if (toFile.exists()) {
+			if (!overwrite) {
+				return;
+			}
+			
+		}
+		
+		createParentDirectories(toFile);
+		
+		if (fromFile.isDirectory()) {
+			
+			ShieldAwareCopier copier = new ShieldAwareCopier(toFile);
+			
+			copier.copy(fromFile);
+			
+		}
+		else {
+			if (ShieldUtils.isShielded(toFile)) {
+				PrintUtils.printInfo("Ignoring shilded file " + to);
+			}
+			else { 
+				FileUtils.copyFile(fromFile, toFile);
+			}
+		}
+		
+	}
+	
+	
+	/**
+	 * Creates a directory
+	 * @param directory the directory to create
+	 */
+	public static void mkdir(final String directory) {
+		File dir = new File(directory);
+		
+		if (!dir.exists()) { 
+			dir.mkdirs();
+		}
+		else {
+			PrintUtils.printInfo("Directory " + directory + " already exists");
+		}
+	}
+
+	
+
+}
