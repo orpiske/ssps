6178acb40b32643e8a9d1043953736566981faa2
Otavio R. Piske <angusyoung@gmail.com>
2013-03-03 14:47:36 -0300

    Utility class to instantiate a new db manager

---
+++ src/net/orpiske/ssps/common/db/derby/DerbyManagerFactory.java
@@ -0,0 +1,36 @@
+/**
+   Copyright 2012 Otavio Rodolfo Piske
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package net.orpiske.ssps.common.db.derby;
+
+import java.util.Properties;
+
+import net.orpiske.ssps.common.db.exceptions.DatabaseInitializationException;
+import net.orpiske.ssps.common.utils.Utils;
+
+/**
+ * @author Otavio R. Piske <angusyoung@gmail.com>
+ *
+ */
+public class DerbyManagerFactory {
+	
+	public static DerbyDatabaseManager newInstance() throws DatabaseInitializationException {
+		Properties props = System.getProperties();
+		props.setProperty("derby.system.home", Utils.getSdmDirectoryPath());
+		
+		return new DerbyDatabaseManager("registry", props);
+	}
+	
+}
