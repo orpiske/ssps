ccdfef1bb722803f3e05cbbe886df19d5319d679
Otavio R. Piske <angusyoung@gmail.com>
2013-03-08 09:29:32 -0300

    Fixed the zip unpacker: it was not closing resources, throwing exceptions because the output path/directory was not prepared accordingly and reorganized the code

--- src/net/orpiske/ssps/common/archive/zip/ZipArchive.java
+++ src/net/orpiske/ssps/common/archive/zip/ZipArchive.java
@@ -17,10 +17,12 @@ package net.orpiske.ssps.common.archive.zip;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.zip.ZipException;
 
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipFile;
@@ -39,6 +41,35 @@ import net.orpiske.ssps.common.archive.exceptions.SspsArchiveException;
 public class ZipArchive implements Archive {
 
 	
+	private long extractFile(ZipFile zipFile, ZipArchiveEntry entry,
+			File outFile) throws IOException, ZipException,
+			FileNotFoundException {
+		InputStream fin = null;
+		BufferedInputStream bin = null;
+		FileOutputStream out = null;
+
+		long ret = 0;
+		
+		try { 
+			fin = zipFile.getInputStream(entry);
+			bin = new BufferedInputStream(fin);
+			out = new FileOutputStream(outFile);
+
+			ret += IOUtils.copy(fin, out);
+			
+			IOUtils.closeQuietly(out);
+			IOUtils.closeQuietly(bin);
+			IOUtils.closeQuietly(fin);
+		}
+		finally {
+			IOUtils.closeQuietly(out);
+			IOUtils.closeQuietly(bin);
+			IOUtils.closeQuietly(fin);
+		}
+		
+		return ret;
+	}
+	
 	/**
 	 * Uncompress a file
 	 * @param source the source file to be uncompressed
@@ -46,42 +77,40 @@ public class ZipArchive implements Archive {
 	 * @return the number of bytes read
 	 * @throws IOException for lower level I/O errors
 	 */
-	public static long unpack(File source, File destination) throws IOException {
-		ZipFile zipFile = new ZipFile(source);
+	public long unpack(File source, File destination) throws IOException {
+		ZipFile zipFile = null; 
+		long ret = 0;
 		
-		Enumeration<ZipArchiveEntry> entries = zipFile.getEntries();
-		ZipArchiveEntry entry = entries.nextElement();
-		
-		while (entry != null) {
-			File outFile = new File(destination.getPath() + File.separator 
-					+ entry.getName());
+		try { 
+			zipFile = new ZipFile(source);
+			Enumeration<ZipArchiveEntry> entries = zipFile.getEntries();
 			
-			CompressedArchiveUtils.prepareDestination(destination);
+			while (entries.hasMoreElements()) { 
+				ZipArchiveEntry entry = entries.nextElement();
 			
-			if (!entry.isDirectory()) {
-				InputStream fin = zipFile.getInputStream(entry);
-				BufferedInputStream bin = new BufferedInputStream(fin);
+				File outFile = new File(destination.getPath() + File.separator 
+						+ entry.getName());
 				
+				CompressedArchiveUtils.prepareDestination(outFile);
 				
-				FileOutputStream out = new FileOutputStream(outFile);
+				if (!entry.isDirectory()) {
+					ret += extractFile(zipFile, entry, outFile);
+				}
 				
-		
-				IOUtils.copy(fin, out);
-				
-				IOUtils.closeQuietly(out);
-			
-				IOUtils.closeQuietly(fin);
-				IOUtils.closeQuietly(bin);
+				int mode = entry.getUnixMode();
+				PermissionsUtils.setPermissions(mode, outFile);
 			}
-			
-			int mode = entry.getUnixMode();
-			PermissionsUtils.setPermissions(mode, outFile);
-		
-			entry = entries.nextElement();
+		}
+		finally { 
+			if (zipFile != null) { 
+				zipFile.close();
+			}
 		}
 		
-		return 0;
+		return ret;
 	}
+
+	
 
 	@Override
 	public long unpack(String source, String destination)
@@ -89,15 +118,16 @@ public class ZipArchive implements Archive {
 		
 		File sourceFile = new File(source);
 		File destinationDir = new File(destination);
+		long ret = 0;
 		
 		try {
-			unpack(sourceFile, destinationDir);
+			ret = unpack(sourceFile, destinationDir);
 		} catch (IOException e) {
 			throw new SspsArchiveException("Unable to unpack: " 
 					+ e.getMessage(), e);
 		}
 		
-		return 0;
+		return ret;
 	}
 
 }
