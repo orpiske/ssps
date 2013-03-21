eb738724126ec759d2fc2b51622b1d8d3e97590b
Otavio R. Piske <angusyoung@gmail.com>
2013-03-21 16:28:37 -0300

    Re-added because we need to be able to read the script information in order to support dependencies

---
+++ src/net/orpiske/ssps/common/repository/utils/PackageDataUtils.java
@@ -0,0 +1,91 @@
+/**
+   Copyright 2012 Otavio Rodolfo Piske
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package net.orpiske.ssps.common.repository.utils;
+
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.orpiske.ssps.common.repository.PackageInfo;
+import net.orpiske.ssps.common.repository.exception.PackageInfoException;
+
+import org.apache.log4j.Logger;
+import org.codehaus.groovy.control.CompilationFailedException;
+
+/**
+ * The Groovy script engine
+ * 
+ * @author Otavio R. Piske <angusyoung@gmail.com>
+ */
+public class PackageDataUtils {
+	
+	private static Logger logger = Logger.getLogger(PackageDataUtils.class);
+	
+	private PackageDataUtils() {
+		
+	}
+	
+	private static GroovyObject getObject(final File file) throws PackageInfoException {
+		ClassLoader parent = PackageDataUtils.class.getClassLoader();
+		GroovyClassLoader loader = new GroovyClassLoader(parent);
+		
+		Class<?> groovyClass;
+		try {
+			groovyClass = loader.parseClass(file);
+		} catch (CompilationFailedException e) {
+			throw new PackageInfoException("The script has errors: " + e.getMessage(),
+					e);
+		} catch (IOException e) {
+			throw new PackageInfoException("Input/output error: " + e.getMessage(),
+					e);
+		}
+
+		GroovyObject groovyObject;
+		try {
+			groovyObject = (GroovyObject) groovyClass.newInstance();
+		} catch (InstantiationException e) {
+			throw new PackageInfoException("Unable to instantiate object: " 
+					+ e.getMessage(), e);
+		} catch (IllegalAccessException e) {
+			throw new PackageInfoException("Illegal access: " + e.getMessage(),
+					e);
+		}
+		
+		return groovyObject;
+	}
+		
+
+	/*
+	 * (non-Javadoc)
+	 * @see net.orpiske.sdm.engine.Engine#run(java.io.File)
+	 */
+	public static void read(final File file, final PackageInfo packageInfo) throws PackageInfoException {
+		GroovyObject groovyObject = getObject(file);
+	
+		String name = groovyObject.getProperty("name").toString();
+		String version = groovyObject.getProperty("version").toString();
+		String slot = groovyObject.getProperty("slot").toString();
+		String url = groovyObject.getProperty("url").toString();
+		
+		
+		packageInfo.setName(name);
+		// packageInfo.setVersion(version);
+		packageInfo.setUrl(url);
+		packageInfo.setSlot(slot);
+	}	
+}
