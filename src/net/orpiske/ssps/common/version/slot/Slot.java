095dedfcf73cd47f0b5061e29d7d3c9ec3bf1d16
Otavio R. Piske <angusyoung@gmail.com>
2013-03-21 11:19:30 -0300

    Code cleanup: added comments

--- src/net/orpiske/ssps/common/version/slot/Slot.java
+++ src/net/orpiske/ssps/common/version/slot/Slot.java
@@ -18,28 +18,46 @@ package net.orpiske.ssps.common.version.slot;
 import net.orpiske.ssps.common.version.Version;
 
 /**
+ * Abstracts the slot information
  * @author Otavio R. Piske <angusyoung@gmail.com>
  *
  */
 public class Slot {
 	private SlotComparator slotComparator;
 	
+	/**
+	 * Constructor
+	 */
 	public Slot() {
 		slotComparator = SlotComparatorFactory.create(SlotComparatorFactory.DEFAULT_SLOT);
 	}
 	
 	
+	/**
+	 * Constructor
+	 * @param slotComparator The slot comparator
+	 */
 	public Slot(SlotComparator slotComparator) {
 		this.slotComparator = slotComparator;
 	}
 	
+	/**
+	 * Constructor
+	 * @param slotMask The slot mask string
+	 */
 	public Slot(final String slotMask) {
 		slotComparator = SlotComparatorFactory.create(slotMask);
 	}
 	
 	
+	/**
+	 * Whether one version share the same slot as the other 
+	 * @param v1 The version to check against
+	 * @param v2 The version to check
+	 * @return True if v2 fits/shares the same slot as v1.
+	 */
 	public boolean fits(Version v1, Version v2) {
-		return slotComparator.fits(v1.getValue(), v2.getValue());
+		return slotComparator.fits(v1.toString(), v2.toString());
 	}
 
 }
