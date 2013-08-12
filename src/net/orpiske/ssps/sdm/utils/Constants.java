3661f10d7e2e2e5e4e711908d3b75916540a8a9e
Otavio R. Piske <angusyoung@gmail.com>
2013-08-12 10:14:28 -0300

    Fixed the version string

--- src/net/orpiske/ssps/sdm/utils/Constants.java
+++ src/net/orpiske/ssps/sdm/utils/Constants.java
@@ -24,7 +24,7 @@ import java.io.File;
  */
 public final class Constants {
 	
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.2.2";
 	
 	/**
 	 * Restricted constructor
