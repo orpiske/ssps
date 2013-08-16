6e9ba5d984c71bd6e0da29e4f19cf6faefd18609
Otavio R. Piske <angusyoung@gmail.com>
2013-08-16 15:01:21 -0300

    Added support for loading plugins

--- src/net/orpiske/ssps/common/repository/utils/PackageDataUtils.java
+++ src/net/orpiske/ssps/common/repository/utils/PackageDataUtils.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 
+import net.orpiske.ssps.common.groovy.GroovyClasspathHelper;
 import net.orpiske.ssps.common.repository.PackageInfo;
 import net.orpiske.ssps.common.repository.exception.PackageInfoException;
 
@@ -31,26 +32,25 @@ import org.codehaus.groovy.runtime.metaclass.MissingPropertyExceptionNoStack;
 
 /**
  * The Groovy script engine
- * 
+ *
  * @author Otavio R. Piske <angusyoung@gmail.com>
  */
 public class PackageDataUtils {
-	
+
 	private static Logger logger = Logger.getLogger(PackageDataUtils.class);
-	
+
 	private PackageDataUtils() {
-		
+
 	}
-	
+
 	private static GroovyObject getObject(final File file) throws PackageInfoException {
-		ClassLoader parent = PackageDataUtils.class.getClassLoader();
-		GroovyClassLoader loader = new GroovyClassLoader(parent);
-		
+		GroovyClasspathHelper classpathHelper = GroovyClasspathHelper.getInstance();
+		GroovyClassLoader loader = classpathHelper.getLoader();
+
 		Class<?> groovyClass;
 		try {
-			loader.addClasspath(file.getParent() + File.separator + "files");
-			loader.addClasspath(file.getParent());
-			
+			loader.addClasspath(file.getParent() + File.separator + "resources");
+
 			groovyClass = loader.parseClass(file);
 		} catch (CompilationFailedException e) {
 			throw new PackageInfoException("The script has errors: " + e.getMessage(),
@@ -59,31 +59,29 @@ public class PackageDataUtils {
 			throw new PackageInfoException("Input/output error: " + e.getMessage(),
 					e);
 		}
-		
 
 		GroovyObject groovyObject;
 		try {
 			groovyObject = (GroovyObject) groovyClass.newInstance();
 		} catch (InstantiationException e) {
-			throw new PackageInfoException("Unable to instantiate object: " 
+			throw new PackageInfoException("Unable to instantiate object: "
 					+ e.getMessage(), e);
 		} catch (IllegalAccessException e) {
 			throw new PackageInfoException("Illegal access: " + e.getMessage(),
 					e);
 		}
-		
-		
+
 		return groovyObject;
 	}
-		
-	
+
+
 	private static boolean isLib(final File file) {
-		File parent = file.getParentFile(); 
-		
+		File parent = file.getParentFile();
+
 		if (parent.getName().equals("local") || parent.getName().equals("files")) {
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -92,24 +90,24 @@ public class PackageDataUtils {
 	 * @see net.orpiske.sdm.engine.Engine#run(java.io.File)
 	 */
 	public static void read(final File file, final PackageInfo packageInfo) throws PackageInfoException {
-		
+
 		if (isLib(file)) {
 			return;
 		}
-		
-		
+
+
 		GroovyObject groovyObject = getObject(file);
-	
+
 		String url = groovyObject.getProperty("url").toString();
-		
-		try { 
+
+		try {
 			Object o = groovyObject.getProperty("dependencies");
-						
-			if (o instanceof LinkedHashMap) { 
+
+			if (o instanceof LinkedHashMap) {
 				@SuppressWarnings("unchecked")
 				LinkedHashMap<String, String> dependencies =
 					(LinkedHashMap<String, String>) o;
-				
+
 				packageInfo.setDependencies(dependencies);
 			}
 		}
@@ -119,5 +117,5 @@ public class PackageDataUtils {
 		}
 
 		packageInfo.setUrl(url);
-	}	
+	}
 }
