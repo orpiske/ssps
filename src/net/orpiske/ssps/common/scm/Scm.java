1b6c8f41ed68dde53e077078c6c81dc9777ee535
Otavio R. Piske <angusyoung@gmail.com>
2013-09-13 13:02:23 -0300

    Implemented the SCM logic separately so that it can be reused by other tools, as well as the script library

---
+++ src/net/orpiske/ssps/common/scm/Scm.java
@@ -0,0 +1,54 @@
+/**
+ Copyright 2013 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+package net.orpiske.ssps.common.scm;
+
+import net.orpiske.ssps.common.scm.exceptions.DuplicateCheckoutException;
+import net.orpiske.ssps.common.scm.exceptions.ScmCheckoutException;
+import net.orpiske.ssps.common.scm.exceptions.ScmUpdateException;
+
+import java.io.File;
+
+/**
+ * SCM Interface
+ */
+public interface Scm {
+
+	/**
+	 * Set the login credentials
+	 * @param credentials the login credentials
+	 */
+	void setCredentials(final ScmCredentials credentials);
+
+	/**
+	 * Sets the branch to work with
+	 * @param branch the branch name
+	 */
+	void setBranch(final ScmBranch branch);
+
+	/**
+	 * Check-out data from a repository
+	 * @param url the URL as a String object
+	 * @param path the path to checkout to              
+	 */
+	void checkout(final String url, File path) throws ScmCheckoutException, DuplicateCheckoutException;
+
+	/**
+	 * Update data in a local repository
+	 * @param path the path as a String object
+	 */
+	void update(final String path) throws ScmUpdateException;
+	
+}
---
+++ src/net/orpiske/ssps/common/scm/exceptions/ScmAccessException.java
@@ -0,0 +1,44 @@
+/**
+   Copyright 2012 Otavio Rodolfo Piske
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package net.orpiske.ssps.common.scm.exceptions;
+
+import net.orpiske.ssps.common.exceptions.SspsException;
+
+/**
+ * @author Otavio R. Piske <angusyoung@gmail.com>
+ *
+ */
+@SuppressWarnings("serial")
+public class ScmAccessException extends SspsException {
+
+	/**
+	 * @param message
+	 */
+	public ScmAccessException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param t
+	 */
+	public ScmAccessException(String message, Throwable t) {
+		super(message, t);
+		// TODO Auto-generated constructor stub
+	}
+
+}
