7cc5ce8d1aa76b056f56d78d141a6310183780bb
Otavio R. Piske <angusyoung@gmail.com>
2013-10-08 16:37:18 -0300

    Added support for user-provided overrides, which allow the user to set a different path instead of the ~/.sdm directory

--- src/net/orpiske/ssps/common/utils/Utils.java
+++ src/net/orpiske/ssps/common/utils/Utils.java
@@ -29,17 +29,35 @@ public class Utils {
 	
 	private static String USER_LOCAL_DIRECTORY = ".sdm";
 	
+	private static String USER_HOME = null;
+	
 	/**
 	 * Restricted constructor
 	 */
 	private Utils() {}
 
+
+	private static String getUserHomeOrOverride() {
+		String userHome = System.getenv("USER_LOCAL_DIRECTORY");
+
+		if (userHome == null) {
+			userHome = FileUtils.getUserDirectoryPath();
+		}
+
+		return userHome + File.separator + USER_LOCAL_DIRECTORY;
+	}
+
+
 	/**
-	 * Gets the user SDM directory path (ie.: $HOME/.sdm)
+	 * Gets the user SDM directory path (ie.: $HOME/.sdm) or the appropriate override
 	 * @return the user SDM directory path
 	 */
 	public static String getSdmDirectoryPath() {
-		return FileUtils.getUserDirectoryPath() + File.separator + USER_LOCAL_DIRECTORY;
+		if (USER_HOME == null) {
+			USER_HOME = getUserHomeOrOverride();
+		}
+		
+		return USER_HOME;
 	}
 	
 	
